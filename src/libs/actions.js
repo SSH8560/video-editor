60b904fc9f0f58fb11f0c1a39eab77a094f911d9
ssh8560 <ssh8560@naver.com>
2024-03-12 11:38:41 +0900

    사용안하는 함수 제거

--- src/libs/actions.js
+++ src/libs/actions.js
@@ -1,46 +1,5 @@
 import { clientId, proejctUrl, scope } from "./constants";
 
-export const showVideoPicker = (accessToken, onPicked) => {
-  const { gapi, google } = window;
-
-  gapi.load("picker", () => {
-    const view = new google.picker.View(google.picker.ViewId.DOCS);
-    view.setMimeTypes("video/mp4");
-
-    const picker = new google.picker.PickerBuilder()
-      .enableFeature(google.picker.Feature.NAV_HIDDEN)
-      .setOrigin("http://localhost:3000")
-      .setOAuthToken(accessToken)
-      .addView(view)
-      .setCallback(onPicked)
-      .build();
-
-    picker.setVisible(true);
-  });
-};
-
-export const getAccessToken = (accessToken, onTakeToken) => {
-  const tokenClient = window.google.accounts.oauth2.initTokenClient({
-    client_id: clientId,
-    scope: scope,
-    callback: async (response) => {
-      console.log(response);
-      if (response.error) {
-        throw response;
-      }
-      if (response.access_token) {
-        onTakeToken(response.access_token);
-      }
-    },
-  });
-
-  if (accessToken) {
-    tokenClient.requestAccessToken({ prompt: "" });
-  } else {
-    tokenClient.requestAccessToken({ prompt: "consent" });
-  }
-};
-
 export const fetchBlob = async (accessToken, fileId) => {
   try {
     const url = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`;
